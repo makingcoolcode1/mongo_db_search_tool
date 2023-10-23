d735a40f3c6fa0750133756d7bcd5d675b5cf198
Zach <zachgedge@gmail.com>
2023-10-23 18:36:59 -0400

    Added a search field option

--- mongo_terminal_search.py
+++ mongo_terminal_search.py
@@ -1,56 +1,59 @@
 
 from pymongo import MongoClient
-import time
 
 
 def format_mongo(document, prefix = ""):
-    formatted = ""
+    formtted = ""
+
 
     for key, value in document.items():
         new_prefix = f"{prefix}.{key}" if prefix else key
 
         if isinstance(value, dict):
-            formatted += format_mongo(value, new_prefix)
+            formtted += format_mongo(value, new_prefix)
         elif isinstance(value, list):
-            formatted += f"{new_prefix}\n"
+            formtted += f"{new_prefix}\n"
 
 
             for item in value:
                 if isinstance(item, dict):
-                    formatted += format_mongo(item, new_prefix)
+                    formtted += format_mongo(item, new_prefix)
                 else:
-                    formatted += f"                 {item}\n"
+                    formtted += f"           {item}\n"
         
         else:
-            formatted += f"{new_prefix}: {value}\n\n"
+            formtted += f"{new_prefix}: {value}\n\n"
     
-    return formatted
+    return formtted
 
-client_inp = input("Enter mongo.db connection string ")
-db_inp = input("Enter db name ")
-collection_input = input("Enter colleciton name ")
+
+client_inp = input("Enter your mongo.db connection string ")
+db_inp = input("Enter your mongo.db database name ")
+collection_inp = input("Enter your mongo.db collection name ")
 
 
 client = MongoClient(f"{client_inp}")
-db = client[f'{db_inp}']
-collection = db[f'{collection_input}']
+db = client[f"{db_inp}"]
+collection = db[f"{collection_inp}"]
 
 
 while True:
-    query = input("Enter a query or type exit to quit ")
+    criteria_inp = input("Enter the search field or type exit to quit ")
+    query = input("Enter your search criteria ")
 
-    if query.lower() == 'exit':
+    if query.lower() == "exit":
         break
 
     try:
-        cursor = collection.find({"recipe_name": query})
+        collection_search = collection.find({criteria_inp: query})
 
-        for document in cursor:
-            formtted_document = format_mongo(document)
-            print(formtted_document)
+        for document in collection_search:
+
+
+            formatted_document = format_mongo(document)
             print()
+            print(formatted_document)
+            print()
+
     except Exception as e:
-        print(f"ERROR: {str(e)}")
-
-
-client.close()
+        print(f"ERROR: {str}")
