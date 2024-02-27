18d916b0ef35fb82926fd05dfafc619a759c38dc
zgedge1 <zachgedge@gmail.com>
2024-02-26 19:33:03 -0500

    adding loop to reset code if user entered wrong connection

--- mongo_terminal_search.py
+++ mongo_terminal_search.py
@@ -1,42 +1,32 @@
 
 from pymongo import MongoClient
 
-print("\n**WELCOME TO THE MONGO.DB SEARCH TOOL**")
-print("\nYou may type 'quit' at anytime to exit the program ")
+print("** WELCOME TO THE MONGO.DB SEARCH TOOL **")
 
+connected = False
 
-clientInput = input("\nEnter your mongo.db connection string")
-dbInput = input("\nEnter your database name to search")
-collectionInput = input("\nEnter your collection to search")
+while not connected:
+    clientInput = input("\nEnter your mongo.db connection string: ")
+    dbInput = input("\nEnter your database name: ")
+    collectionInput = input("\nEnter your collection name: ")
 
-
-try:
-    clientTest = MongoClient(clientInput)
-    dbTest = clientTest.get_database(dbInput)
-    collectionTest = dbTest.get_collection(collectionInput)
-
-    collection_name = dbTest.list_collection_names()
-
-    if collection_name:
-        print("\n Successfully Connected to Mongodb Database..")
-        print("Collections in database are:", collection_name)
-    else:
-        print("Successfully connected to the database but the database is empty")
-except Exception as e:
-    print("\nERROR: Failed to connect to the database")
-    print(f"\n ERROR: {str(e)}")
-
-client = (f"{clientInput}")
-db = [f"{dbInput}"]
-collectionInput = [f"{collectionInput}"]
-
-
-while True:
-    operatorInput = input("Enter a mongo.db operator to search: ")
-    if operatorInput.lower() == "exit":
-        exit("Exiting Program.....")
     
-    queryInput = input("Enter a search query")
-    if queryInput.lower() == "exit":
-        exit("Exiting Program......")
-        
+    try:
+
+        clientTest = MongoClient(clientInput)
+        dbTest = clientTest.get_database(dbInput)
+        collectionTest = dbTest.get_collection(collectionInput)
+
+        testConnection = dbTest.list_collection_names()
+
+        if testConnection:
+            print("\nSuccessfully connected to the mongo.db database")
+            print("\nCollections in the database include: ", testConnection)
+            connected = True
+        else:
+            print("\nSuccessfully connected to the database but there are no collections or the database name is invalid!")
+            connected = True
+    except Exception as e:
+        print("\nERROR: Could not connect to the database!")
+        print(f"\n ERROR: {str(e)}")
+        connected = False
